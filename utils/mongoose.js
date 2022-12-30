70f372354947e44af004041dfc5cc085cc09d0f8
Chelle77322 <serenynadarra@gmail.com>
2022-12-30 15:44:10 +1030

    Delete utils directory

--- utils/mongoose.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/
-no-anonymous-default-export */
-
-import mongoose, {connect} from 'mongoose';
-
-export default (DB_NAME) => {
-    connect(`mongodb://localhost/${DB_NAME}`);
-}
-console.log(mongoose);
